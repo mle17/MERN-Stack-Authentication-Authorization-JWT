7d060f1cafcf5c67cda36f7349b258e9b2647dcb
Michael Vu Le <michael.vu.le96+github@gmail.com>
2021-10-17 15:11:05 -0700

    Prettify js code.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 app.use(cookieParser());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mernauth',{useNewUrlParser : true,useUnifiedTopology: true},()=>{
-    console.log('successfully connected to database');
-});
+mongoose.connect(
+  "mongodb://localhost:27017/mernauth",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("successfully connected to database");
+  }
+);
 
-const userRouter = require('./routes/User');
-app.use('/user',userRouter);
+const userRouter = require("./routes/User");
+app.use("/user", userRouter);
 
-app.listen(5000,()=>{
-    console.log('express server started');
+app.listen(5000, () => {
+  console.log("express server started");
 });
