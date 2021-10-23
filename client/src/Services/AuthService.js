8747521061742c99d64b511c8bb2f2d347bd3686
Michael Vu Le <michael.vu.le96+github@gmail.com>
2021-10-23 11:15:39 -0700

    Refactor services with await and status ok code.

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -1,38 +1,52 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-  login: (user) => {
+  login: async (user) => {
     console.log(user);
-    return fetch("/user/login", {
+    const res = await fetch("/user/login", {
       method: "post",
       body: JSON.stringify(user),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => {
-      if (res.status !== 401) return res.json().then((data) => data);
-      else return { isAuthenticated: false, user: { username: "", role: "" } };
     });
+    
+    return await getUserResponse(res);
   },
-  register: (user) => {
+  register: async (user) => {
     console.log(user);
-    return fetch("/user/register", {
+    const res = await fetch("/user/register", {
       method: "post",
       body: JSON.stringify(user),
       headers: {
         "Content-Type": "application/json",
       },
-    })
-      .then((res) => res.json())
-      .then((data) => data);
-  },
-  logout: () => {
-    return fetch("/user/logout")
-      .then((res) => res.json())
-      .then((data) => data);
-  },
-  isAuthenticated: () => {
-    return fetch("/user/authenticated").then((res) => {
-      if (res.status !== 401) return res.json().then((data) => data);
-      else return { isAuthenticated: false, user: { username: "", role: "" } };
     });
+    const data = await res.json();
+    return data;
+  },
+  logout: async () => {
+    const res = await fetch("/user/logout");
+    const data = await res.json();
+    return data;
+  },
+  isAuthenticated: async () => {
+    const res = await fetch("/user/authenticated");
+    
+    return await getUserResponse(res);
   },
 };
+
+let getUserResponse = async (/** @type {Response} */ response) => {
+  if (response.ok) {
+    return await response.json();
+  }
+  else {
+    return { 
+      isAuthenticated: false, 
+      user: { 
+        username: "", 
+        role: "" 
+      }
+    };
+  }
+}
--- client/src/Services/TodoService.js
+++ client/src/Services/TodoService.js
@@ -1,22 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-  getTodos: () => {
-    return fetch("/user/todos").then((response) => {
-      if (response.status !== 401) {
-        return response.json().then((data) => data);
-      } else return { message: { msgBody: "UnAuthorized", msgError: true } };
-    });
+  getTodos: async () => {
+    const response = await fetch("/user/todos");
+
+    return await getResponse(response);
   },
-  postTodo: (todo) => {
-    return fetch("/user/todo", {
+  postTodo: async (/** @type {any} */ todo) => {
+    const response = await fetch("/user/todo", {
       method: "post",
       body: JSON.stringify(todo),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((response) => {
-      if (response.status !== 401) {
-        return response.json().then((data) => data);
-      } else return { message: { msgBody: "UnAuthorized" }, msgError: true };
     });
+
+    return await getResponse(response);
   },
 };
+
+let getResponse = async (/** @type {Response} */ response) => {
+  if (response.ok) {
+    return await response.json();
+  }
+  else {
+    return { 
+      message: { 
+        msgBody: `Error Status Code: ${response.status}, ${response.statusText}.}`, 
+        msgError: true,
+      }
+    };
+  }
+}
