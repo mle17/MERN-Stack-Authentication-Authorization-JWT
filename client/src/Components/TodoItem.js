9e4189cdfce43bb8b13b16a5b0ec79557dccfbe0
Michael Vu Le <michael.vu.le96+github@gmail.com>
2021-10-23 14:22:48 -0700

    Add delete button UI.

--- client/src/Components/TodoItem.js
+++ client/src/Components/TodoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const TodoItem = (props) => {
-  return <li>{props.todo.name}</li>;
+  return (
+    <>
+      <li>{props.todo.name}</li>
+      <Button variant="danger">Delete</Button>
+    </>);
 };
 
 export default TodoItem;
