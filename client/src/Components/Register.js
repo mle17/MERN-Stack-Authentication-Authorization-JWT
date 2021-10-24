502f33f5d7dae0227a1f189c204ae2ac0637ccb8
Michael Vu Le <michael.vu.le96+github@gmail.com>
2021-10-24 15:00:15 -0700

    Use react bootstrap for user registration.

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,6 +1,8 @@
+// @ts-nocheck
 import React, { useState, useRef, useEffect } from "react";
 import AuthService from "../Services/AuthService";
 import Message from "../Components/Message";
+import { Button, Form } from "react-bootstrap";
 
 const Register = (props) => {
   const [user, setUser] = useState({ username: "", password: "", role: "" });
@@ -37,45 +39,57 @@ const Register = (props) => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <h3>Please Register</h3>
-        <label htmlFor="username" className="sr-only">
-          Username:{" "}
-        </label>
-        <input
-          type="text"
-          name="username"
-          value={user.username}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Username"
-        />
-        <label htmlFor="password" className="sr-only">
-          Password:{" "}
-        </label>
-        <input
-          type="password"
-          name="password"
-          value={user.password}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Password"
-        />
-        <label htmlFor="role" className="sr-only">
-          Role:{" "}
-        </label>
-        <input
-          type="text"
-          name="role"
-          value={user.role}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter role (admin/user)"
-        />
-        <button className="btn btn-lg btn-primary btn-block" type="submit">
+      <Form onSubmit={onSubmit}>
+        <h3>Registration</h3>
+
+        {/* Username */}
+        <Form.Group>
+          <Form.Label htmlFor="username" className="sr-only">
+            Username
+          </Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={onChange}
+            className="form-control"
+            placeholder="Enter Username"
+          />
+        </Form.Group>
+
+        {/* Password */}
+        <Form.Group>
+          <Form.Label htmlFor="password" className="sr-only">
+            Password
+          </Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={onChange}
+            className="form-control"
+            placeholder="Enter Password"
+          />
+        </Form.Group>
+
+        {/* User Role */}
+        <Form.Group>
+          <Form.Label htmlFor="role" className="sr-only">
+            Role
+          </Form.Label>
+          <Form.Control
+            type="text"
+            name="role"
+            value={user.role}
+            onChange={onChange}
+            className="form-control"
+            placeholder="Enter role (admin/user)"
+          />
+        </Form.Group>
+        <Button className="btn btn-lg btn-primary btn-block" type="submit">
           Register
-        </button>
-      </form>
+        </Button>
+      </Form>
       {message ? <Message message={message} /> : null}
     </div>
   );
